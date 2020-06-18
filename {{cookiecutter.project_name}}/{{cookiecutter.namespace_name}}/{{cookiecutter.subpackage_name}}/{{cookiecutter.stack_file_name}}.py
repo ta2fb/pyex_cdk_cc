0b63f4d7273e874bf60305e78764b16125174612
Thomas Adam <tadam@biocorellc.com>
2020-06-18 09:04:12 -0400

    update documentation for stack file

--- {{cookiecutter.project_name}}/{{cookiecutter.namespace_name}}/{{cookiecutter.subpackage_name}}/{{cookiecutter.stack_file_name}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.namespace_name}}/{{cookiecutter.subpackage_name}}/{{cookiecutter.stack_file_name}}.py
@@ -1,9 +1,24 @@
+"""Document file"""
+
 from aws_cdk import core
 
 
 class {{cookiecutter.stack_name}}(core.Stack):
+    """Main CDK stack documentation
+
+    This project uses the Google Style Python Docstrings
+
+    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+    """
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        """
+
+        Args:
+            scope: Scope
+            id: Unique ID
+            **kwargs: Extra keyword argument list
+        """
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
