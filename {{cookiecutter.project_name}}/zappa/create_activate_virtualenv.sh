ea321bbc254b92fd6c1fb88035a23c0fd13bd523
Thomas Adam <tadam@biocorellc.com>
2021-10-07 11:36:07 -0400

    remove zappa directory

--- {{cookiecutter.project_name}}/zappa/create_activate_virtualenv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-ENV_NAME='{{cookiecutter.namespace_name}}.{{cookiecutter.subpackage_name}}_zappa_venv'
-
-if [ ! -d $ENV_NAME ]; then
-    virtualenv $ENV_NAME
-fi
-source ${ENV_NAME}/bin/activate
-pip install -r requirements.txt
